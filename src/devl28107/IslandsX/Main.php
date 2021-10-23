065ee3814c97f30be6b73ef618a72865effd03df
Dev925 <70399224+Dev925@users.noreply.github.com>
2021-10-22 23:27:39 -0500

    onEnable func

--- src/devl28107/IslandsX/Main.php
+++ src/devl28107/IslandsX/Main.php
@@ -1,1 +1,19 @@
+<php
+
+namespace devl28107\IslandsX;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\Player;
+
+class Main extends PluginBase {
+    
+    public function onEnable(){
+        $this->getServer()->getPluginManager()->registerEvents($this,$this);
+        $this->getLogger()->info("Plugin Enabled");
+    }
+}
 
